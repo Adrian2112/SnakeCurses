79bc03ad989631af47bf5a82dca4a6ce39d463ce
Adrian Gonzalez <bernardogzzf@gmail.com>
2014-04-12 16:27:46 -0500

    display snake in window

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <ncurses.h>
+#include <stdio.h>
  
 #define WORLD_WIDTH 50
 #define WORLD_HEIGHT 20
+#define TICKRATE 100
+
+#define SNAKE_INITIAL_LENGTH 3
+
+typedef struct SnakePart {
+  int x;
+  int y;
+} SnakePart;
 
 WINDOW *create_world();
+void initialize_snake(SnakePart *snake, int snake_size);
+void move_snake(WINDOW *window, SnakePart *snake, int snake_size);
  
 int main(int argc, char *argv[]) {
  
     WINDOW *world;
+    SnakePart snake[SNAKE_INITIAL_LENGTH];
  
     initscr();
     noecho();
@@ -18,12 +30,19 @@ int main(int argc, char *argv[]) {
     refresh();
  
     world = create_world();
+    int snake_size = sizeof(snake)/sizeof(SnakePart);
+    initialize_snake(snake, snake_size);
  
     box(world, 0 , 0);
  
+    move_snake(world, snake, snake_size);
+
     wrefresh(world);
  
-    getch();
+    int ch;
+    while ((ch = getch()) != 'q')
+    {
+    }
  
     delwin(world);
  
@@ -39,4 +58,20 @@ WINDOW *create_world()
   int offsetY = (LINES - WORLD_HEIGHT) / 2;
 
   return newwin(WORLD_HEIGHT, WORLD_WIDTH, offsetY, offsetX);
+}
+
+void initialize_snake(SnakePart *snake, int snake_size){
+   int i;
+   for (i = 0; i < snake_size; i++) {
+     snake[i].x = i+1;
+     snake[i].y = 1;
+   }
+}
+
+void move_snake(WINDOW *win, SnakePart *snake, int snake_size)
+{
+  int i;
+  for (i = 0; i < snake_size; i++) {
+    mvwaddch(win, snake[i].y, snake[i].x, '#');
+  }
 }
